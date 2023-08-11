e89dd9608aa769c6dff8de21ed3e4e414c048797
Proger187 <141851773+Proger187@users.noreply.github.com>
2023-08-11 10:50:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
+const {router} = require("./routes/index")
 const port = process.env.PORT
 const sequelize = require("./DB")
 // const {router} = require("./routes/index")
 
 app.use(cors())
 app.use(express.json())
+app.use("/api", router)
 // app.use("/api", router)
 
 async function start(){
